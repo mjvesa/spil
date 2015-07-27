1bc2f5cf28adda2f46fdac1482fd5f9670ba42f0
Matti Vesa <mjvesa@vaadin.com>
2015-07-27 12:52:55 +0300

    #3 Fix example code after vaadin-widget improvements

--- examples/component.scm
+++ examples/component.scm
@@ -4,32 +4,32 @@
 (define-macro (basic-label text)
   `(widget
     (client-init
-     (element-append-child! root-element (element-new '(div ,text))))))
+     (append-to-root (element-new '(div ,text))))))
 
 (define-macro (modifiable-label text)
   `(let ((component (widget
 		     (client-init
 		      (define text-div (element-new '(div ,text)))
 		      (element-append-child! root-element text-div))
-		     (client-rpc set-text
-				 (js-set! text-div "innerHTML" "value goes here")))))
+		     (client-rpc (set-text value)
+				 (js-set! text-div "innerHTML" value)))))
      (lambda (op)
        (case op
 	 ((component) component)
-	 ((set-text) (lambda (x) (call-client component 'set-text)))))))
+	 ((set-text) (lambda (text) (call-client-rpc component 'set-text text)))))))
 
 
 (define component
   (widget
    (client-init      
-     ((define canvas
+      (define canvas
 	(element-new '(canvas)))
-      (element-append-child! (js-invoke self "getElement") canvas)
+      (element-append-child! root-element canvas)
       (define ctx
 	(js-invoke canvas "getContext" "2d"))
       (js-set! ctx "fillStyle" "blue")
-      (js-invoke ctx "fillRect" 10 10 100 100)))
-    (client-rpc poks
+      (js-invoke ctx "fillRect" 10 10 100 100))
+    (client-rpc (poks)
 		(begin
 		  (console-warn "I was called")
 		  (js-set! ctx "fillStyle" "green")
@@ -37,8 +37,8 @@
 
 
 (define (main ui)
-  (let ((label (modifiable-label "whoa dude")))    
+  (let ((label (modifiable-label "Old text")))    
     (.addComponent ui (label 'component))
-    ((label 'set-text) "stuff")
-    (.addComponent ui (basic-label "<b>This is not modifiable</b>"))))
-
+    ((label 'set-text) "\"New text\"")
+    (.addComponent ui (basic-label "<b>This is not modifiable</b>"))
+    (.addComponent ui component)))
