6a742d7956539b3647951480b529610146e958b5
Matti Vesa <mjvesa@vaadin.com>
2015-07-31 11:23:36 +0300

    #5 Move widget client side code into a closure

--- scm/vaadin-widget.scm
+++ scm/vaadin-widget.scm
@@ -1,53 +1,72 @@
 ;; Widget macro and related helper procedures
 
 (define client-boilerplate
-  '(begin (define namespace "com_github_mjvesa_spil_SchemeComponent")
-	  (define self (js-eval (string-append namespace ".self")))
-	  (define root-element (js-invoke self "getElement"))
-	  (define (list-to-string lst)
+  '(let*
+	 ((namespace "com_github_mjvesa_spil_SchemeComponent")
+	  (self (js-eval (string-append namespace ".self")))
+	  (root-element (js-invoke self "getElement"))
+	  (list-to-string (lambda (lst)
 	    (let ((out (open-output-string)))
 		  (write lst out)
-		  (get-output-string out)))
-	  (define (call-server name params)
+		  (get-output-string out))))
+	  (call-server (lambda name params)
 	    (js-invoke self (symbol->string name)  (list-to-string params)))
-	  (define (get-state)
-	    (js-invoke self "getState"))
-	  (define (append-to-root element)
-	    (element-append-child! root-element element))))
+	  (get-state (lambda ()
+	    (js-invoke self "getState")))
+	  (append-to-root (lambda (element)
+			    (element-append-child! root-element element))))))
 
 (define (client-function params code)
-  (cons 'begin
-	(cons client-boilerplate
-	      `((js-set! self ,(.toString (car params))
-			 (js-closure
-			  (lambda (str) (let ((,@(cdr params) (read (open-input-string str))))
-					      ,code))))))))
+  `((js-set! self ,(.toString (car params))
+	     (js-closure
+	      (lambda (str) (let ((,@(cdr params) (read (open-input-string str))))
+			      ,code))))))
 
 (define (client-state-function code)
-  (cons 'begin
-	(cons client-boilerplate
-	      `((js-set! self "onStateChange"
-			 (js-closure
-			  (lambda () ,@code)))))))
+  `((js-set! self "onStateChange"
+	     (js-closure
+	      (lambda () ,@code)))))
+
+(define client-code '())
 
 (define (handle-widget-section comp section)
   (let ((param-name (cdadr section))
 	(section-code (caddr section)))
     (case (car section)
       ((client-init)
-       (.setComponentCode comp (.toString (cons 'begin (cons client-boilerplate (cdr section))))))
+       (set! client-code (append client-code (cdr section))))
       ((client-rpc)
-       (.setComponentCode comp (.toString (client-function (cadr section) (cddr section)))))
+       (set! client-code (append client-code (client-function (cadr section) (cddr section)))))
+      ((on-state-change)
+       (set! client-code (append client-code (client-state-function (cdr section)))))
       ((server-rpc)
        (.registerServerRpc  comp (symbol->string (caadr section)) (eval `(lambda ,param-name ,section-code))))
-      ((on-state-change)
-       (.setComponentCode comp  (.toString (client-state-function (cdr section)))))
       (else
        (display (string-append "Unrecognized section: " (car section) "\n"))))))
 
+(define (upload-macros comp)
+  (.setComponentCode comp (.toString
+
+			   
+			   '(define-macro (client-rpc params code)
+			      `(js-set! self ,(symbol->string (car params))
+					 (js-closure
+					  (lambda (str) (let ((,@(cdr params) (read (open-input-string str))))
+							  ,code)))))))
+  (.setComponentCode comp (.toString
+
+			   '(define-macro (on-state-change code)
+			      `(js-set! self "onStateChange"
+					 (js-closure
+					  (lambda () ,@code)))))))
+
 (define-macro  (widget . widget-definition) 
   `(let ((comp (SchemeComponent.)))
+     (upload-macros comp)
+     (set! client-code client-boilerplate)
      (for-each (lambda (def) (handle-widget-section comp  def)) ',widget-definition)
+     (display (string-append "Client code:\n" (list (append '(lambda ()) (list client-code))) "\n"))
+     (.setComponentCode comp (.toString (list (append '(lambda ()) (list client-code)))))
      comp))
 
 (define (call-client comp func)
@@ -58,3 +77,5 @@
 
 (define (set-widget-state comp state)
   (.setLst (.getState comp) (.toString state)))
+
+
--- src/main/resources/com/github/mjvesa/spil/scheme_component.js
+++ src/main/resources/com/github/mjvesa/spil/scheme_component.js
@@ -1,6 +1,6 @@
 com_github_mjvesa_spil_SchemeComponent = function() {
 
-    var biwascheme = new BiwaScheme.Interpreter(function(e) { console.log(e.message); })
+    var biwascheme = new BiwaScheme.Interpreter(function(e) { console.error(e.message); })
     
     this.setComponentCode = function(code) {
 	// This is a horrible hack to access this from Scheme
