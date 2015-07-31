f39be79b8cfd0d28d94694e93d010bcbd08d70c9
Matti Vesa <mjvesa@vaadin.com>
2015-07-31 13:16:54 +0300

    #5 some fixes to examples and vaadin-widget

---
+++ examples/component/main.scm
@@ -0,0 +1,94 @@
+;; Custom component examples
+(load "vaadin.scm")
+
+;; Labels are not complex
+(define-macro (basic-label text)
+  `(widget
+    (client-init
+     (append-to-root (element-new '(div ,text))))))
+
+;; Label with client and server RPC, state change listening and API using
+;; message passing
+(define-macro (modifiable-label text)
+  `(let ((component (widget
+		     (client
+		      (define text-div (element-new '(div ,text)))
+		      (element-append-child! root-element text-div)
+		      (client-rpc (settext value)
+				  (js-set! text-div "innerHTML" value)
+				  (call-server 'testi '(test list)))
+		      (on-state-change
+		       (js-set! text-div "innerHTML"
+				(list-to-string (get-state)))))
+		     (server-rpc (testi text)
+				 (display (.toString text))))))
+     (lambda (op)
+       (case op
+	 ((component) component)
+	 ((settext) (lambda (text) (call-client-rpc component 'settext text)))))))
+
+
+;; Drawing canvas with configurable client and server RPCs
+(define-macro (drawing-canvas cli-rpc serv-rpc target-rpc color)
+  `(widget
+    (client
+     (let*
+	 ((canvas
+	   (element-new '(canvas width "200" height "200")))
+	  (ctx
+	   (js-invoke canvas "getContext" "2d"))
+	  (set-color (lambda  (color)
+		       (js-set! ctx "fillStyle" color)))
+	  (rect (lambda (x y w h color)
+		  (set-color color)
+		  (js-invoke ctx "fillRect" x y w h))))
+       
+       (element-append-child! root-element canvas)
+       (rect 0 0 200 200 "gray")
+       (client-rpc ,@cli-rpc)
+       (js-invoke canvas "addEventListener" "mousemove"
+		  (js-closure
+		   (lambda (ev)
+		     (let ((x (js-ref ev "offsetX"))
+			   (y (js-ref ev "offsetY")))
+		       (rect x y 10 10 ,color)
+		       (if (not (eq? ,target-rpc '()))
+			   (begin
+			     (call-server ,target-rpc (list x y))))))))))
+    (server-rpc ,@serv-rpc)))
+
+(define main-layout '())
+
+(define (add-label text)
+  (.addComponent main-layout (Label. text)))
+
+(define right-canvas
+  (drawing-canvas
+   ((draw coords) (rect (car coords) (cadr coords) 10 10 "green"))
+   ((a b) (display "nothing much"))
+   '()
+   "red"))
+
+(define left-canvas
+  (drawing-canvas
+   ((a b) (display "nothing much"))
+   ((sendcoords coords) (call-client-rpc right-canvas 'draw (.toString coords)))
+   'sendcoords
+   "blue"))
+
+(define (main ui)
+  (let ((label (modifiable-label "Old text")))
+    (set! main-layout ui)
+    (.addComponent ui (label 'component))
+    ((label 'settext) "\"New text\"")
+    (.addComponent ui (Button. "Change state"
+   			       (VaadinUtil.buttonClickListener (lambda (event)
+   								 (set-widget-state (label 'component) '(This is my state)))))))
+  (.addComponent ui (basic-label "<b>This is not modifiable</b>"))
+
+  (let ((hl (HorizontalLayout.)))
+    (.setSpacing hl #t)
+    (.addComponent hl left-canvas)
+    (.addComponent hl right-canvas)
+    (.addComponent ui hl)))
+
--- scm/vaadin-widget.scm
+++ scm/vaadin-widget.scm
@@ -9,36 +9,20 @@
 	    (let ((out (open-output-string)))
 		  (write lst out)
 		  (get-output-string out))))
-	  (call-server (lambda name params)
-	    (js-invoke self (symbol->string name)  (list-to-string params)))
+	  (call-server (lambda (name paramz)
+		       (js-invoke self (symbol->string name)  (list-to-string paramz))))
 	  (get-state (lambda ()
 	    (js-invoke self "getState")))
 	  (append-to-root (lambda (element)
 			    (element-append-child! root-element element))))))
-
-(define (client-function params code)
-  `((js-set! self ,(.toString (car params))
-	     (js-closure
-	      (lambda (str) (let ((,@(cdr params) (read (open-input-string str))))
-			      ,code))))))
-
-(define (client-state-function code)
-  `((js-set! self "onStateChange"
-	     (js-closure
-	      (lambda () ,@code)))))
-
 (define client-code '())
 
 (define (handle-widget-section comp section)
   (let ((param-name (cdadr section))
 	(section-code (caddr section)))
     (case (car section)
-      ((client-init)
+      ((client)
        (set! client-code (append client-code (cdr section))))
-      ((client-rpc)
-       (set! client-code (append client-code (client-function (cadr section) (cddr section)))))
-      ((on-state-change)
-       (set! client-code (append client-code (client-state-function (cdr section)))))
       ((server-rpc)
        (.registerServerRpc  comp (symbol->string (caadr section)) (eval `(lambda ,param-name ,section-code))))
       (else
@@ -46,8 +30,6 @@
 
 (define (upload-macros comp)
   (.setComponentCode comp (.toString
-
-			   
 			   '(define-macro (client-rpc params code)
 			      `(js-set! self ,(symbol->string (car params))
 					 (js-closure
@@ -62,10 +44,9 @@
 
 (define-macro  (widget . widget-definition) 
   `(let ((comp (SchemeComponent.)))
-     (upload-macros comp)
+     (upload-macros comp) ;; TODO needs to be part of the app init
      (set! client-code client-boilerplate)
      (for-each (lambda (def) (handle-widget-section comp  def)) ',widget-definition)
-     (display (string-append "Client code:\n" (list (append '(lambda ()) (list client-code))) "\n"))
      (.setComponentCode comp (.toString (list (append '(lambda ()) (list client-code)))))
      comp))
 
